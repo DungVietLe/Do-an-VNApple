aeb4430805fb03e153a168d6936ce335d93159df
Khuong Duy <langkhuongduy2001@gmail.com>
2021-06-24 06:16:31 +0700

    Duy add

--- Danh Sach Nav/BOS/bos.js
+++ Danh Sach Nav/BOS/bos.js
@@ -8,19 +8,43 @@ var ArraycolorImage = [
    
 function angleleftFun(){
     var Valueimg = document.getElementById('Imagesdirectionalvalueimg')
-    if (Valueimg.src.match("/images/iphone-xr-bos.png")) {
-        Valueimg.src = "/images/iphone-xr-red-bos.png";
+    console.log(Valueimg.getAttribute('src'))
+    var checksrc = Valueimg.getAttribute('src') 
+    if (checksrc == '/images/iphone-xr-bos.png') {
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-red-bos.png')
     }
-    else if (Valueimg.src.match("/images/iphone-xr-red-bos.png")) {
-        Valueimg.src = "/images/iphone-xr-red-bos.png";
+    else if(Valueimg.getAttribute('src') == '/images/iphone-xr-red-bos.png'){
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-blue-bos.png')
     }
-    else if(Valueimg.src.match("/images/iphone-xr-red-bos.png")){
-        Valueimg.src = '/images/iphone-xr-yellow-bos.png';
+    else if (Valueimg.getAttribute('src') =='/images/iphone-xr-blue-bos.png') {
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-yellow-bos.png')
     }
     else{
-        Valueimg.src = "/images/iphone-xr-bos.png"
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-bos.png')
     }
-
+}
 function anglerightFun(){
-
+    var Valueimg = document.getElementById('Imagesdirectionalvalueimg')
+    console.log(Valueimg.getAttribute('src'))
+    var checksrc = Valueimg.getAttribute('src') 
+    if (checksrc == '/images/iphone-xr-bos.png') {
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-yellow-bos.png')
+    }
+    else if(Valueimg.getAttribute('src') == '/images/iphone-xr-yellow-bos.png'){
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-blue-bos.png')
+    }
+    else if (Valueimg.getAttribute('src') =='/images/iphone-xr-blue-bos.png') {
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-red-bos.png')
+    }
+    else{
+        Valueimg.removeAttribute('src')
+        Valueimg.setAttribute('src', '/images/iphone-xr-bos.png')
+    }
 }
