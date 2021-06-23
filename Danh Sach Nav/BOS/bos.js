d13c6e156e1dbe7864ef7e93d1e8a0af08b4fdca
Khuong Duy <langkhuongduy2001@gmail.com>
2021-06-24 02:09:13 +0700

    duy add

--- Danh Sach Nav/BOS/bos.js
+++ Danh Sach Nav/BOS/bos.js
@@ -4,21 +4,23 @@ var ArraycolorImage = [
     '/images/iphone-xr-yellow-bos.png',
     '/images/iphone-xr-bos.png'
 ]
+    
+   
+function angleleftFun(){
     var Valueimg = document.getElementById('Imagesdirectionalvalueimg')
-    // console.log(Valueimg)
-    var angleleft = document.getElementById('angleleft')
-    var adngleright = document.getElementById('angleright')
-//    angleleft.onclick = function(){
-//        for(let i = 0; i<ArraycolorImage.length;i++){
-//             Valueimg.setAttribute('src', ArraycolorImage[i])
-//             if(Valueimg.src === ArraycolorImage[i]){
-//                 Valueimg.remove('src')
-//             }
-//        }
-//     }
-
-    angleleft.onclick = function(){
-        for(let i = 0; i < ArraycolorImage[i]; i++)
-        console.log(ArraycolorImage[i])
-        // Valueimg.src = ArraycolorImage[]
+    if (Valueimg.src.match("/images/iphone-xr-bos.png")) {
+        Valueimg.src = "/images/iphone-xr-red-bos.png";
     }
+    else if (Valueimg.src.match("/images/iphone-xr-red-bos.png")) {
+        Valueimg.src = "/images/iphone-xr-red-bos.png";
+    }
+    else if(Valueimg.src.match("/images/iphone-xr-red-bos.png")){
+        Valueimg.src = '/images/iphone-xr-yellow-bos.png';
+    }
+    else{
+        Valueimg.src = "/images/iphone-xr-bos.png"
+    }
+
+function anglerightFun(){
+
+}
