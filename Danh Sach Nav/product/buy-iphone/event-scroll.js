aff0e5532b45173d39f2cd716839bdcc561e229b
LE DUNG <dungvietle1299@gmail.com>
2021-06-24 17:17:01 +0700

    fix

---
+++ Danh Sach Nav/product/buy-iphone/event-scroll.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector(".header-nav-buyPC");
+let changeName = document.querySelector(".title-name-productPC");
+window.addEventListener("scroll", function() {
+    var x = pageYOffset;
+    if (x >= 12) {
+        // nav.style.display = "none";
+        changeName.style.top = "0";
+    } else if (x <= 12) {
+        // nav.style.display = "flex";
+        changeName.style.top = "44px";
+    }
+})
