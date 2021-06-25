553a53dae920d13feec5f1caa5823cf873e39564
Khuong Duy <langkhuongduy2001@gmail.com>
2021-06-25 15:56:26 +0700

    duy add

--- Danh Sach Nav/KhamPha/khampha.js
+++ Danh Sach Nav/KhamPha/khampha.js
@@ -2,23 +2,18 @@ var OnMac = document.querySelector('.OnMac')
 var OffMac = document.querySelector('.OffMac')
 var mac = document.getElementById('MAC')
 var imac = document.getElementById('IMAC')
-OnMac.onclick = function(){
-    if(mac.style.visibility == 'hidden'){
-        imac.style.visibility = 'hidden'
-        mac.style.visibility = ''
-    }
-    else{
-        imac.style.visibility = ''
-        mac.style.visibility = 'hidden'
-    }
-}
-OffMac.onclick = function(){
-    if(imac.style.visibility == 'hidden'){
-        imac.style.visibility = ''
-        mac.style.visibility = 'hidden'
-    }
-    else{
-        imac.style.visibility = 'hidden'
-        mac.style.visibility = ''
-    }
-}
+OnMac.addEventListener("click", function(e) {
+    e.preventDefault()
+        mac.classList.remove('animate__fadeOut')
+        mac.classList.add('animate__fadeIn')
+        imac.classList.remove('animate__fadeIn')
+        imac.classList.add('animate__fadeOut')
+})
+
+OffMac.addEventListener("click", function(e) {
+    e.preventDefault()
+        mac.classList.remove('animate__fadeIn')
+        mac.classList.add('animate__fadeOut')
+        imac.classList.remove('animate__fadeOut')
+        imac.classList.add('animate__fadeIn')
+})
