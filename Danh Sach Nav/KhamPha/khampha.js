c4dd42619bab3ab4f5301324734f35971ae4cf6c
Khuong Duy <langkhuongduy2001@gmail.com>
2021-06-25 17:50:48 +0700

    Duy add

--- Danh Sach Nav/KhamPha/khampha.js
+++ Danh Sach Nav/KhamPha/khampha.js
@@ -4,6 +4,9 @@ var mac = document.getElementById('MAC')
 var imac = document.getElementById('IMAC')
 OnMac.addEventListener("click", function(e) {
     e.preventDefault()
+        mac.style.visibility = 'visible'
+        imac.style.visibility = 'hidden'
+        
         mac.classList.remove('animate__fadeOut')
         mac.classList.add('animate__fadeIn')
         imac.classList.remove('animate__fadeIn')
@@ -12,8 +15,10 @@ OnMac.addEventListener("click", function(e) {
 
 OffMac.addEventListener("click", function(e) {
     e.preventDefault()
+        imac.style.visibility = 'visible'
+        mac.style.visibility = 'hidden'
         mac.classList.remove('animate__fadeIn')
-        mac.classList.add('animate__fadeOut')
+        // mac.classList.add('animate__fadeOut')
         imac.classList.remove('animate__fadeOut')
         imac.classList.add('animate__fadeIn')
 })
