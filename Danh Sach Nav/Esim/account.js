454de3f5c2fb55acfe7bca614c8546853f5b5484
LE DUNG <dungvietle1299@gmail.com>
2021-06-26 15:18:40 +0700

    fix

--- Danh Sach Nav/Esim/account.js
+++ Danh Sach Nav/Esim/account.js
@@ -3,6 +3,34 @@ let messageError = document.querySelector(".message-error");
 let checkInputPass = document.querySelector("#checkinputPass");
 let messageErrorPasss = document.querySelector(".message-errorPass")
 let changeMessage = document.querySelector(".errorPass");
+let checkDangki = document.querySelector("#checkDangKi");
+let checkPassDangki = document.querySelector("#checkPassDangki");
+let msDangKi = document.querySelector(".message-errorDangki");
+let msPassDangKi = document.querySelector(".message-errorPassDangki");
+let linkDangKi = document.querySelector(".creat-idPC");
+let containerDangNhap = document.querySelector(".signin-box-PC");
+let containerDangKi = document.querySelector(".regis-box-PC");
+let btnDangki = document.querySelector("#btn-DangKi");
+
+
+let userDangki = [];
+btnDangki.addEventListener("click", function() {
+    console.log(checkDangKi.value);
+    console.log(checkPassDangki.value);
+})
+
+
+
+
+
+linkDangKi.addEventListener("click", function() {
+    if (containerDangNhap.classList.contains("off") === false) {
+        containerDangNhap.classList.add("off")
+    }
+    if (containerDangKi.classList.contains("off")) {
+        containerDangKi.classList.remove("off")
+    }
+})
 
 function checkValueName() {
 
@@ -40,6 +68,45 @@ function checkValuePass() {
         messageErrorPasss.classList.remove("off");
         checkInputPass.style.border = "1px solid #e30000";
         checkInputPass.style.backgroundColor = "#fff2f4";
+
+    }
+}
+
+function checkValueDangKi() {
+    if (checkDangki.value === "") {
+        msDangKi.classList.remove("off");
+        checkDangki.style.border = "1px solid #e30000";
+        checkDangki.style.backgroundColor = "#fff2f4";
+
+    }
+    checkDangki.addEventListener("click", function() {
+        msDangKi.classList.add("off");
+        checkDangki.style.border = "1px solid #d2d2d7";
+        checkDangki.style.backgroundColor = "#fff";
+        // checkValueInput.style.boxShadow = "0 0 0 4px rgb(0 125 250 / 60%)";
+        // checkValueInput.style.outLine = "none"
+    })
+}
+
+function checkValuePassDangKi() {
+    if (checkPassDangki.value === "") {
+        msPassDangKi.classList.remove("off");
+        checkPassDangki.style.border = "1px solid #e30000";
+        checkPassDangki.style.backgroundColor = "#fff2f4";
+
+    }
+    checkPassDangki.addEventListener("click", function() {
+        msPassDangKi.classList.add("off");
+        checkPassDangki.style.border = "1px solid #d2d2d7";
+        checkPassDangki.style.backgroundColor = "#fff";
+        // checkValueInput.style.boxShadow = "none";
+        // checkValueInput.style.outLine = "none"
+    })
+    if (checkPassDangki.value.length >= "1" && checkPassDangki.value.length < "6") {
+        changeMessage.innerText = "Please enter than 6 value"
+        msPassDangKi.classList.remove("off");
+        checkPassDangki.style.border = "1px solid #e30000";
+        checkPassDangki.style.backgroundColor = "#fff2f4";
 
     }
 }
