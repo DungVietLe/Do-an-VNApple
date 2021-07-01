f3180ca43c259c7bf0c90d0a9919f3761a2c9978
LE DUNG <dungvietle1299@gmail.com>
2021-07-01 17:35:14 +0700

    fix

--- Danh Sach Nav/KhamPha/Mac/Mac.js
+++ Danh Sach Nav/KhamPha/Mac/Mac.js
@@ -1,4 +1,4 @@
-let nav = document.querySelector(".header-nav-buyPC");
+// let nav = document.querySelector(".header-nav-buyPC");
 let changeName = document.querySelector(".title-name-productPC");
 window.addEventListener("scroll", function() {
     var x = pageYOffset;
@@ -19,6 +19,10 @@ let colorGold = document.querySelector("#color-product-GoldPC");
 let colorBlue = document.querySelector("#color-product-BluePC")
 let macpropro = document.querySelector('.macpropro')
 let urldaylensanpam = 'https://sheetdb.io/api/v1/8eo9evgklp5lb';
+let checkColor;
+let checkUrlMac;
+let checkName;
+let checkPrice;
 chooseIphone12pr.addEventListener("click", function(e) {
     e.preventDefault()
     images.src = "/images/khampha/macbook13pro.png";
@@ -29,24 +33,29 @@ chooseIphone12pr.addEventListener("click", function(e) {
     if (chooseIphone12.style.border = "1px solid royalblue") {
         chooseIphone12.style.border = "1px solid #d2d2d7"
     }
-    if (macpropro.style.border ="1px solid royalblue") {
+    if (macpropro.style.border = "1px solid royalblue") {
         macpropro.style.border = "1px solid #d2d2d7"
     }
     changePrice.innerText = '1299$ USD'
-    if(Color){
+    if (Color) {
         newProduct = {
-       name: "Mac Book Air",
-       coin: '999$ USD',
-       color: 'Graphite'
-       }
-   }
-   else{
-       newProduct = {
-       name: "Mac Book Air",
-       coin: '999$ USD',
-       color: 'Sliver'
-       }
-   }
+            tensanpham: "Mac Book Air",
+            giasanpham: '999$ USD',
+            color: 'Graphite',
+            url: "/images/khampha/macbook13pro.png"
+        }
+    } else {
+        newProduct = {
+            tensanpham: "Mac Book Air",
+            giasanpham: '999$ USD',
+            color: 'Sliver',
+            url: "/images/khampha/macbook13pro.png"
+        }
+    }
+    checkName = "Macbook Pro 13";
+    checkPrice = "1299";
+    checkUrlMac = "/images/khampha/macbook13pro.png";
+
 })
 chooseIphone12.addEventListener("click", function(e) {
     e.preventDefault()
@@ -58,25 +67,30 @@ chooseIphone12.addEventListener("click", function(e) {
     if (chooseIphone12pr.style.border = "1px solid royalblue") {
         chooseIphone12pr.style.border = "1px solid #d2d2d7"
     }
-    if (macpropro.style.border ="1px solid royalblue") {
+    if (macpropro.style.border = "1px solid royalblue") {
         macpropro.style.border = "1px solid #d2d2d7"
     }
     changePrice.innerText = '999$ USD'
-    if(Color){
-         newProduct = {
-        name: "Mac Pro 13 ",
-        coin: '1299$ USD',
-        color: 'Graphite'
-        }
-    }
-    else{
+    if (Color) {
         newProduct = {
-        name: "Mac Pro 13",
-        coin: '1299$ USD',
-        color: 'Sliver'
+            tensanpham: "Mac Pro 13 ",
+            giasanpham: '1299$ USD',
+            color: 'Graphite',
+            url: "/images/khampha/macbook13pro.png"
+        }
+    } else {
+        newProduct = {
+            tensanpham: "Mac Pro 13",
+            giasanpham: '1299$ USD',
+            color: 'Sliver',
+            url: "/images/khampha/macbook13pro.png"
+
         }
     }
-   
+    checkName = "Macbook Air";
+    checkPrice = "999";
+    checkUrlMac = "/images/khampha/macbook13pro.png";
+
 })
 macpropro.addEventListener("click", function(e) {
     e.preventDefault()
@@ -91,21 +105,26 @@ macpropro.addEventListener("click", function(e) {
         macpropro.style.border = "1px solid royalblue"
     }
     changePrice.innerText = '2999$ USD'
-    if(Color){
+    if (Color) {
         newProduct = {
-       name: "Mac Book 16 Pro",
-       coin: '2999$ USD',
-       color: 'Graphite'
-       }
-   }
-   else{
-       newProduct = {
-        name: "Mac Book 16 Pro",
-        coin: '2999$ USD',
-       color: 'Sliver'
-       }
-   }
-    
+            tensanpham: "Mac Book 16 Pro",
+            giasanpham: '2999$ USD',
+            color: 'Graphite',
+            url: "/images/khampha/macbook13-1.png"
+        }
+    } else {
+        newProduct = {
+            tensanpham: "Mac Book 16 Pro",
+            giasanpham: '2999$ USD',
+            color: 'Sliver',
+            url: "/images/khampha/macbook13-1.png"
+
+        }
+    }
+    checkName = "Macbook Pro 16";
+    checkPrice = "2999";
+    checkUrlMac = "/images/khampha/macbook13-1.png"
+
 })
 
 colorGraphite.addEventListener("click", function(e) {
@@ -118,6 +137,7 @@ colorGraphite.addEventListener("click", function(e) {
         images.src = "/Danh Sach Nav/KhamPha/Mac/imgMac/space-select.jfif";
     }
     Color = true;
+    checkColor = "Space Gray"
 })
 colorSliver.addEventListener("click", function(e) {
     e.preventDefault()
@@ -129,57 +149,55 @@ colorSliver.addEventListener("click", function(e) {
         images.src = "/Danh Sach Nav/KhamPha/Mac/imgMac/mac1-sliver.jfif";
     }
     Color = false;
+    checkColor = "Sliver"
 })
 
 var changePrice = document.querySelector('#changePrice-ipPC')
-console.log(changePrice.innerText)
+
 
 
 //theem sanr pham 
 
-var productArray = [
-]
-var newProduct = {
-}
+
 var Color = true;
 customBTN = document.querySelector('.btn-3')
-
-customBTN.onclick = function(){
-    if(chooseIphone12.style.border = "1px solid royalblue") {
-        chooseIphone12.style.border = "1px solid #d2d2d7"
-        chooseIphone12pr.style.border = "1px solid #d2d2d7"
-        macpropro.style.border ="1px solid #d2d2d7"
-        colorGraphite.style.border = "1px solid #d2d2d7"
-        colorSliver.style.border = "1px solid #d2d2d7";
-        productArray.push(newProduct)
-    }
-    else if (chooseIphone12pr.style.border = "1px solid royalblue") {
+let productApiMac = "http://localhost:3000/product";
+customBTN.onclick = function() {
+    if (chooseIphone12.style.border = "1px solid royalblue") {
         chooseIphone12.style.border = "1px solid #d2d2d7"
         chooseIphone12pr.style.border = "1px solid #d2d2d7"
         macpropro.style.border = "1px solid #d2d2d7"
         colorGraphite.style.border = "1px solid #d2d2d7"
         colorSliver.style.border = "1px solid #d2d2d7";
-        productArray.push(newProduct)
-    }
-    else{
+
+    } else if (chooseIphone12pr.style.border = "1px solid royalblue") {
         chooseIphone12.style.border = "1px solid #d2d2d7"
         chooseIphone12pr.style.border = "1px solid #d2d2d7"
-        macpropro.style.border ="1px solid #d2d2d7"
+        macpropro.style.border = "1px solid #d2d2d7"
         colorGraphite.style.border = "1px solid #d2d2d7"
         colorSliver.style.border = "1px solid #d2d2d7";
-        productArray.push(newProduct)
-    }
-    console.log(productArray)
-    var lengarray = productArray.length;
-    axios.post(urldaylensanpam, { tensanpham: productArray[lengarray-1].name , dongia:productArray[lengarray-1].coin , mau:productArray[lengarray-1].color }).then((response) => console.log(response));
-    confirm("Registered Successfully");
-    setTimeout(function(){
-            location.href = '/giohang/giohang.html'
-    },300)
-    
-}
 
-// customBTN.addEventListener('click',function(e){
-//     e.preventDefault();
-//     location.href = '/giohang/giohang.html'
-// })
+    } else {
+        chooseIphone12.style.border = "1px solid #d2d2d7"
+        chooseIphone12pr.style.border = "1px solid #d2d2d7"
+        macpropro.style.border = "1px solid #d2d2d7"
+        colorGraphite.style.border = "1px solid #d2d2d7"
+        colorSliver.style.border = "1px solid #d2d2d7";
+
+    }
+
+    fetch(productApiMac, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            tensanpham: checkName,
+            giasanpham: checkPrice,
+            color: checkColor,
+            url: checkUrlMac
+        })
+
+    })
+
+}
