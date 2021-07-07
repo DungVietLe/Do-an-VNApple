98b3da04660a4c527407b79f899d4608fe58ef10
dung <dungvietle@gmail.com>
2021-07-07 14:21:06 +0700

    fix

--- app.js
+++ app.js
@@ -14,52 +14,7 @@ let iconStyleEnd = document.querySelector(".line-bars-end");
 let listNavMobile = document.querySelector(".list-nav-mobile");
 let iconBag = document.querySelector(".icon-bags-mb");
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-var server_host = process.env.YOUR_HOST || "0.0.0.0";
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
-  },
-  devServer: {
-    disableHostCheck: true,
-    contentBase: "./ dist",
-    compress: true,
-    inline: true,
-    port: server_port,
-    host: server_host
-
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
-};
 close.addEventListener("click", function (e) {
     e.preventDefault()
     for (let i = 0; i < elNav.length; i++) {
@@ -250,3 +205,10 @@ fetch("http://localhost:3000/user")
         }
     })
 
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function () {
+    console.log("Listening on Port 3000");
+});
