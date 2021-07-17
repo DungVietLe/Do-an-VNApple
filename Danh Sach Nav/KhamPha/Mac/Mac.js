f5050be48f280befa332b22e37b85b6453a081ff
Khuong Duy <langkhuongduy2001@gmail.com>
2021-07-17 11:10:40 +0700

    duy add

--- Danh Sach Nav/KhamPha/Mac/Mac.js
+++ Danh Sach Nav/KhamPha/Mac/Mac.js
@@ -18,14 +18,35 @@ let colorSliver = document.querySelector("#color-product-SliverPC");
 let colorGold = document.querySelector("#color-product-GoldPC");
 let colorBlue = document.querySelector("#color-product-BluePC")
 let macpropro = document.querySelector('.macpropro')
-let urldaylensanpam = 'https://sheetdb.io/api/v1/8eo9evgklp5lb';
+var countmacbook = 0;
 let checkColor;
 let checkUrlMac;
 let checkName;
 let checkPrice;
+var changePrice = document.querySelector('#changePrice-ipPC')
+// render sappham ra danh sach
+    function renderlist(){
+       fetchmacbook(0)
+    }
+
+renderlist();
+function fetchmacbook(countt){
+    fetch("http://localhost:3000/macbook")
+    .then(function (response) {
+         return response.json()
+    })
+    .then(function (data) {
+            images.setAttribute("src", data[countt].url)
+            var Namemacbook = document.querySelector('.Namemacbook')
+            Namemacbook.innerText = data[countt].tensanpham;
+            changePrice.innerText = data[countt].giasanpham;
+            localStorage.setItem('macbookid', data[countt].id)
+    })
+}
+
 chooseIphone12pr.addEventListener("click", function(e) {
     e.preventDefault()
-    images.src = "/images/khampha/macbook13pro.png";
+    fetchmacbook(1)
     if (chooseIphone12pr.style.border = "1px solid #d2d2d7") {
 
         chooseIphone12pr.style.border = "1px solid royalblue"
@@ -36,7 +57,7 @@ chooseIphone12pr.addEventListener("click", function(e) {
     if (macpropro.style.border = "1px solid royalblue") {
         macpropro.style.border = "1px solid #d2d2d7"
     }
-    changePrice.innerText = '1299$ USD'
+    // changePrice.innerText = '1299$ USD'
     if (Color) {
         newProduct = {
             tensanpham: "Mac Book Air",
@@ -59,8 +80,7 @@ chooseIphone12pr.addEventListener("click", function(e) {
 })
 chooseIphone12.addEventListener("click", function(e) {
     e.preventDefault()
-
-    images.src = "/images/khampha/macbook13pro.png"
+    fetchmacbook(0)
     if (chooseIphone12.style.border = "1px solid #d2d2d7") {
         chooseIphone12.style.border = "1px solid royalblue"
     }
@@ -70,7 +90,7 @@ chooseIphone12.addEventListener("click", function(e) {
     if (macpropro.style.border = "1px solid royalblue") {
         macpropro.style.border = "1px solid #d2d2d7"
     }
-    changePrice.innerText = '999$ USD'
+    // changePrice.innerText = '999$ USD'
     if (Color) {
       checkColor = 'Graphite'
     } else {
@@ -83,7 +103,7 @@ chooseIphone12.addEventListener("click", function(e) {
 })
 macpropro.addEventListener("click", function(e) {
     e.preventDefault()
-    images.src = "/images/khampha/macbook13-1.png"
+    fetchmacbook(2)
     if (chooseIphone12.style.border = "1px solid royalblue") {
         chooseIphone12.style.border = "1px solid #d2d2d7"
     }
@@ -93,7 +113,7 @@ macpropro.addEventListener("click", function(e) {
     if (macpropro.style.border = "1px solid #d2d2d7") {
         macpropro.style.border = "1px solid royalblue"
     }
-    changePrice.innerText = '2999$ USD'
+    // changePrice.innerText = '2999$ USD'
     if (Color) {
         newProduct = {
             tensanpham: "Mac Book 16 Pro",
@@ -141,7 +161,7 @@ colorSliver.addEventListener("click", function(e) {
     checkColor = "Sliver"
 })
 
-var changePrice = document.querySelector('#changePrice-ipPC')
+
 
 
 
